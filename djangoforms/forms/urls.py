27ff099f008343027dee4fe4375c192ffcc9cff5
mahendran-narayanan <mahendranNNM@gmail.com>
2020-04-26 19:17:56 +0530

    form added

--- djangoforms/forms/urls.py
+++ djangoforms/forms/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from forms import views
-
+from django.conf.urls import url
+from forms.views import get_name
 urlpatterns=[
 	path('home.html',views.home,name="home"),
+	url(r'^$',views.get_name,name="getname"),
+	path('thanks/',views.thanks,name="thanks"),
 ]
--- djangoforms/forms/views.py
+++ djangoforms/forms/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render,HttpResponse
 from django.views.generic import TemplateView
 
+from django.http import HttpResponseRedirect
+from .forms import NameForm
+
 # Create your views here.
+def get_name(request):
+	if request.method == 'POST':
+		form = NameForm(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('thanks/')
+	else:
+		form = NameForm()
+	return render(request, 'name.html', {'form':form})
+
 def home(request):
 	template = 'home.html'
 	return render(request,'home.html',{})
+
+def thanks(request):
+	return HttpResponse("Data received")
